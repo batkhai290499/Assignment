2b09ada9c278bf5f62b09af082dc5cf43134aa44
KhaiNguyen <khainbgch17459@fpt.edu.vn>
2019-08-26 09:25:44 +0700

    Update productmanage.php

--- productmanage.php
+++ productmanage.php
@@ -6,9 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Manage Product</title>
     <link rel="stylesheet" href="productmanage.css">
+    <title>Table V01</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-    <div class="trang">
+    <!-- <div class="trang">
     <div class="dau">
         <h1>Xin chao</h1>
     </div>
@@ -28,8 +47,27 @@
                     <div class="product-prop product-button">Edit</div>
                     <div class="product-prop product-button">Copy</div>
                     <div class="clear-both"></div>
-                </li>
-                <?php  
+                </li> -->
+                
+            <!-- </ul>
+        </div>
+    </div> -->
+    <div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100">
+					<table>
+						<thead>
+                        <ul>
+                            <tr class="table100-head">
+								<th class="column1">Date</th>
+								<th class="column2">Order ID</th>
+								<th class="column3">Name</th>
+								<th class="column4">Price</th>
+								<th class="column5">Quantity</th>
+								<th class="column6">Total</th>
+							</tr>
+                            <?php  
                 require_once './database.php';
                     foreach ($resultSet as $row) {
                         ?>
@@ -49,8 +87,28 @@
                         </div>
                         <div class="clear-both"></div>  
                     <?php } ?>
-            </ul>
-        </div>
-    </div>
+                        </ul>
+							
+						</thead>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
+
