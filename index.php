9b942bd1fe4c1d1e2289be5a3d41a8b699e22ef1
KhaiNguyen <khainbgch17459@fpt.edu.vn>
2019-08-26 08:03:40 +0700

    1

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log in</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="trang">
+    <div class="dau"></div>
+    <div class="than">
+    <div id="user_login" class="box-content">
+                <h1>Admin Login</h1>
+                <form action="./index.php" method="Post" autocomplete="off">
+                    <label>Username</label></br>
+                    <input type="text" name="username" value="" /><br/>
+                    <label>Password</label></br>
+                    <input type="password" name="password" value="" /></br>
+                    <br>
+                    <button><a href="productmanage.php">Login</a></button>
+                </form>
+            </div>
+    </div>
+    <div class="cuoi"></div>
+</div>
+
+</body>
+</html>
